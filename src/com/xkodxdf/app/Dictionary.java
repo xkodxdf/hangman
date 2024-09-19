202f4f6c9359208cc628ce5e01cb053b4fb49a3d
xkodxdf <sirotapi@bk.ru>
2024-09-19 17:24:41 +0300

    changed the way to read from a file

--- src/com/xkodxdf/app/Dictionary.java
+++ src/com/xkodxdf/app/Dictionary.java
@@ -1,10 +1,11 @@
 package com.xkodxdf.app;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Dictionary {
 
@@ -17,12 +18,9 @@ public class Dictionary {
 
 
     void setWordList() throws IOException {
-        String FILE_PATH = "./src/resources/data.txt";
-        BufferedReader reader = new BufferedReader(new FileReader(FILE_PATH));
-        String word;
-        while ((word = reader.readLine()) != null) {
-            wordList.add(word);
-        }
-        reader.close();
+        final String FILE_PATH = "./src/resources/data.txt";
+        Stream<String> fileStream = Files.lines(Paths.get(FILE_PATH));
+        fileStream.forEach(wordList::add);
+        fileStream.close();
     }
 }
