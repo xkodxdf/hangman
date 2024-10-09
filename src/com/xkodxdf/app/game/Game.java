09a86c9840e63d8ead3159e21cb926de905e47ce
xkodxdf <sirota92@bk.ru>
2024-10-09 20:45:52 +0300

    refactor: change the initGame() method and move to the Game class from Main

--- src/com/xkodxdf/app/game/Game.java
+++ src/com/xkodxdf/app/game/Game.java
@@ -1,6 +1,9 @@
 package com.xkodxdf.app.game;
 
+import com.xkodxdf.app.Config;
 import com.xkodxdf.app.game.display.Display;
+import com.xkodxdf.app.game.display.OutputText;
+import com.xkodxdf.app.util.Utils;
 
 public class Game {
 
@@ -26,5 +29,32 @@ public class Game {
             display.printEndGameMsg();
             logic.askForContinue();
         }
+    }
+
+    public static String getStartLanguage() {
+        String choice;
+        String startLanguage = Config.RUS_FILE_PATH;
+        String startRuGame = "1";
+        String startEngGame = "2";
+        String exit = "3";
+
+        System.out.println(OutputText.START_GAME_MSG);
+        do {
+            System.out.println(OutputText.GAME_INIT_CHOICE);
+            choice = Utils.getInput();
+            if (exit.equals(choice)) {
+                System.exit(0);
+            }
+            if (startEngGame.equals(choice)) {
+                startLanguage = Config.ENG_FILE_PATH;
+                break;
+            }
+            if (startRuGame.equals(choice)) {
+                break;
+            }
+            System.out.println(OutputText.INVALID_INPUT);
+        } while (true);
+
+        return startLanguage;
     }
 }
--- src/com/xkodxdf/app/game/Main.java
+++ src/com/xkodxdf/app/game/Main.java
@@ -1,40 +1,9 @@
 package com.xkodxdf.app.game;
 
-import com.xkodxdf.app.Config;
-import com.xkodxdf.app.game.display.OutputText;
-import com.xkodxdf.app.util.Utils;
-
 public class Main {
 
     public static void main(String[] args) {
-        Game game = Main.initGame();
+        Game game = new Game(Game.getStartLanguage());
         game.start();
-    }
-
-    public static Game initGame() {
-        String initChoice;
-        String initLanguage = Config.RUS_FILE_PATH;
-        String startRuGame = "1";
-        String startEngGame = "2";
-        String exit = "3";
-
-        System.out.println(OutputText.START_GAME_MSG);
-        do {
-            System.out.println(OutputText.GAME_INIT_CHOICE);
-            initChoice = Utils.getInput();
-            if (exit.equals(initChoice)) {
-                System.exit(0);
-            }
-            if (startEngGame.equals(initChoice)) {
-                initLanguage = Config.ENG_FILE_PATH;
-                break;
-            }
-            if (startRuGame.equals(initChoice)) {
-                break;
-            }
-            System.out.println(OutputText.INVALID_INPUT);
-        } while (true);
-
-        return new Game(initLanguage);
     }
 }
