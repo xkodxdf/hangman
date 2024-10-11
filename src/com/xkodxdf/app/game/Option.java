ae13a9b68b587e147e90c54a5652be851c250be5
xkodxdf <sirota92@bk.ru>
2024-10-11 19:01:35 +0300

    add: enum constant

--- src/com/xkodxdf/app/game/Option.java
+++ src/com/xkodxdf/app/game/Option.java
@@ -3,6 +3,7 @@ package com.xkodxdf.app.game;
 public enum Option {
     NEW_RU_GAME("1"), NEW_ENG_GAME("2"),
     NEW_ROUND("1"), SWITCH_LANGUAGE("2"),
+    INPUT_FILE_PATH("1"),
     EXIT("3");
 
     Option(String value) {
