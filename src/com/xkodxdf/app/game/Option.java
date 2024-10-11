27cc68c71409a974322ea9f1ef4a9a2efb711fa0
xkodxdf <sirota92@bk.ru>
2024-10-11 16:07:21 +0300

    add: enum for menu options

---
+++ src/com/xkodxdf/app/game/Option.java
@@ -0,0 +1,17 @@
+package com.xkodxdf.app.game;
+
+public enum Option {
+    NEW_RU_GAME("1"), NEW_ENG_GAME("2"),
+    NEW_ROUND("1"), SWITCH_LANGUAGE("2"),
+    EXIT("3");
+
+    Option(String value) {
+        this.VALUE = value;
+    }
+
+    private final String VALUE;
+
+    public String getValue() {
+        return VALUE;
+    }
+}
